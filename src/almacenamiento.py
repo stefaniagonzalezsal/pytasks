28a086d6a691ec1391ac60f22dd58eaa28dc5ad0
stefaniagonzalezsal <stefaniagonzalez0493@gmail.com>
2026-06-27 21:59:17 -0500

    Completar funciones guardar_tareas y cargar_tareas

---
+++ src/almacenamiento.py
@@ -0,0 +1,13 @@
+import json
+from tarea import Tarea
+
+def guardar_tareas(tareas, ruta="data/tareas.json"):
+    lista_dict = [tarea.to_dict() for tarea in tareas]
+    with open(ruta, "w") as archivo:
+        json.dump(lista_dict, archivo, indent=4)
+
+def cargar_tareas(ruta= "data/tareas.json"):
+    with open(ruta, "r") as archivo:
+        lista_dict = json.load(archivo)
+    tareas = [Tarea.from_dict(dato) for dato in lista_dict]
+    return tareas
